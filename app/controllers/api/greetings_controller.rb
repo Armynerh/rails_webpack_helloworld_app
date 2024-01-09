38dc6a25537aac11821ac0a65b43731f00a0d583
Armynerh <aimenah@outlook.com>
2024-01-09 16:36:56 -0500

    fix linters

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/api/greetings_controller.rb
 class Api::GreetingsController < ApplicationController
   def random
-    render json: { greeting: Message.order("RANDOM()").first.content }
+    render json: { greeting: Message.order('RANDOM()').first.content }
   end
 end
